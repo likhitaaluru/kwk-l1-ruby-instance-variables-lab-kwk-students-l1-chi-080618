33df0a17d433c26821aec555b64595f288d54b01
likhitaaluru <likhitaaluru@gmail.com>
2018-08-10 16:31:11 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,7 +1,7 @@
 # dog.rb
 class Dog
   def name=(dog_name)
-    this_dogs_name = dog_name
+    this_dogs_name=dog_name
   end
   
   def name
@@ -9,5 +9,5 @@ class Dog
   end
 end
 lassie=Dog.new
-lassie.name = "Lassie"
-lassie.name #=> "Lassie"
+lassie.name="Lassie"
+lassie.name#=>"Lassie"
